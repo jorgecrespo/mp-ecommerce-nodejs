a73e070dd31ae24a58698cae1a57d871596ebe7c
George <jorgeluiscrespo@gmail.com>
2021-02-05 18:39:44 -0300

    req.json()

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ app.post('/api/getlink', async (req, res) => {
 
 app.post('/webhook', async (req, res) => {
    console.log('/webhook')
-    console.log('req:: ', req,'fin-req')
+    console.log('req:: ', req.json(),'fin-req')
     console.log('res:: ', res)
         if (req.method === "POST") { 
           let body = ""; 
